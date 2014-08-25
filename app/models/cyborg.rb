dcec0b897867681443ed945ea4eac48a5a651470
ajaysalindra <ajaykumar8645@gamil.com>
2014-08-25 15:05:48 +0530

    validation-insert

--- app/models/cyborg.rb
+++ app/models/cyborg.rb
@@ -1,3 +1,7 @@
 class Cyborg < ActiveRecord::Base
 	attr_accessible :name, :email
+
+	validates :name, presence:  true, uniqueness: true, length: { maximum: 100 }
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
+
