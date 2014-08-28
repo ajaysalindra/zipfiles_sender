299ba804902b1fd199dac2600e31fdfccc66132c
ajaysalindra <ajaykumar8645@gamil.com>
2014-08-28 14:48:47 +0530

    email configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'ajaykumar8645@gmail.com',
+    password:             'salindraajay8745',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
 end
